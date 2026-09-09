bda990d39d265c4ede65d98d1a54d8003f4b82bb
Simon Da Vall <simon.davall@gmail.com>
2026-09-09 11:58:08 +0100

    tau-58: Implemented caching for lookups

---
+++ Taurus.Application/Caching/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace Taurus.Application.Caching;
+
+public interface ICacheService
+{
+    Task<T> GetOrCreateAsync<T>(string key, TimeSpan duration, Func<Task<T>> factory)
+        where T : notnull;
+}
---
+++ Taurus.Infrastructure/Caching/MemoryCacheService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Taurus.Application.Caching;
+
+namespace Taurus.Infrastructure.Caching;
+
+public sealed class MemoryCacheService(IMemoryCache memoryCache) : ICacheService
+{
+    public async Task<T> GetOrCreateAsync<T>(string key, TimeSpan duration, Func<Task<T>> factory)
+        where T : notnull
+    {
+        var value = await memoryCache.GetOrCreateAsync(
+            key,
+            entry => {
+                entry.AbsoluteExpirationRelativeToNow = duration;
+                return factory();
+            });
+
+        return value ?? throw new InvalidOperationException($"Cache factory returned no value for key '{key}'.");
+    }
+}
